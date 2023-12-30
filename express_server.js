1e85c4c149684a5ba6125f924836f3c810ffa657
Rcm <rcm68888@gmail.com>
2023-12-30 08:25:58 -0700

    adding urls :id route

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL };
+  res.render("urls_show", templateVars);
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
