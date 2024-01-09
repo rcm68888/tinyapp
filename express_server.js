594a31905ef372b3833eeea12fc0e5a364457b00
Rcm <rcm68888@gmail.com>
2024-01-09 07:55:58 -0700

    Adding user login and logout functionalities

--- express_server.js
+++ express_server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieSession = require("cookie-session");
+const cookieParser = require("cookie-parser");
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  const { user_id: sessionUserID } = req.session;
+  if (urlsForUser(sessionUserID, urlDatabase)) {
+    const templateVars = {
+      urls: urlsForUser(sessionUserID),
+      user: users[sessionUserID],
+    };
+    res.render("urls_index", templateVars);
+  } else {
+    res.render("urls_index", { urls: {}, user: "" });
+  }
 });
 
 app.get("/urls/new", (req, res) => {
@@ -29,10 +39,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -44,4 +50,8 @@ app.get("/hello", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
