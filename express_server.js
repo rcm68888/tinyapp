6e9f938f5842518ec830d2dad5da23106d7cd380
Rcm <rcm68888@gmail.com>
2024-01-18 09:07:34 -0700

    adding fixes to display error

--- express_server.js
+++ express_server.js
@@ -6,8 +6,8 @@ const bcrypt = require("bcrypt");
 
 const { generateRandomString, emailTaken, userIdFromEmail, urlsForUser, userLoggedIn } = require("./helpers.js");
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: 'userRandomID' },
+  "9sm5xK": { longURL: "http://www.google.com", userID: 'userRandomID' }
 };
 const users = {
   userRandomID: {
